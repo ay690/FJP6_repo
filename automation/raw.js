eaf2f7cc843b72c08b6930ebece516029e641735
Aniket Yadav <aniketyadav690@gmail.com>
2022-04-15 15:37:30 +0530

    Google opened

--- automation/raw.js
+++ automation/raw.js
@@ -1,14 +1,15 @@
 const puppeteer = require("puppeteer");
 
-let browserPromise = puppeteer.launch({headless:false});//jis page ka automation kar rahe ho woh uski visibility ke liye yeh line use karo
+const browserPromise = puppeteer.launch({headless:false});//jis page ka automation kar rahe ho woh uski visibility ke liye yeh line use karo
 
-browserPromise.then(function(browserInstance){ //object name browserInstance it can be given any name
-   console.log("browser is opened"); 
-   let PagePromise = browserInstance.newPage();//will open page in new tab
+browserPromise.then(function(browser){ //object name browserInstance it can be given any name
+//    console.log("browser is opened"); 
+   let PagePromise = browser.newPage();//will open page in new tab
    return PagePromise;
-}).then(function(page){
-    console.log("page is opened");
-    let urlPromise = page.goto('https://www.google.com/');
+}).then(function(browserpage){
+    //page =browserpage[0];
+    // console.log("page is opened");
+    let urlPromise = browserpage.goto('https://www.google.com/');
     return urlPromise
 }).then(function(){
     console.log("google is opened");
