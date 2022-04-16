29c5fe6f525c7a5ee7da258c4bf63e926ef59f91
Aniket Yadav <aniketyadav690@gmail.com>
2022-04-16 17:57:02 +0530

    githubScaping topics in json

--- web_scrapping/githubScrap.js
+++ web_scrapping/githubScrap.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
+const fs = require("fs");
 
 const link = "https://github.com/topics";
 
@@ -29,12 +30,56 @@ function cb2(error, response, html){
     }else{
         const dom = new JSDOM(html);
         const document = dom.window.document;
-        let issues = document.querySelectorAll("flex-auto.min-width-0.p-2.pr-3.pr-md-2");
-        
-        for(let i = 0; i < 6; i++){
-            let issue_names = issues[i].querySelectorAll(".Link--primary.v-align-middle.no-underline.h4.js-navigation-open.markdown-title");
-            console.log(issue_names[i].textContent);
+        let repository = document.querySelectorAll(".text-bold.wb-break-word");
+        for(let i = 0; i < 8; i++){
+            let repository_link = "https://github.com" + repository[i].href;
+            //console.log(repository_link);
+            request(repository_link, cb3);
         }
         
+    }
+}
+
+function cb3(error, response, html){
+    if(error){
+        console.log(error);
+    }else{
+        const dom = new JSDOM(html);
+        const document = dom.window.document;
+        let tab_sec = document.querySelector(".UnderlineNav-body.list-style-none");
+        let tab_list= tab_sec.querySelectorAll("li");
+
+        if(tab_list.length >= 7){
+            let issue = tab_sec.querySelector("#issues-tab");
+            let issue_link = "https://github.com" + issue.href;
+           // console.log(issue_link);
+           request(issue_link, cb4);
+        }
+    }
+}
+function cb4(error, response, html){
+    if(error){
+        console.log(error);
+    }else{
+        const dom = new JSDOM(html);
+        const document = dom.window.document;
+        let allIssues = document.querySelectorAll(".Link--primary.v-align-middle.no-underline.h4.js-navigation-open.markdown-title");
+        if(allIssues.length > 5){
+            for(let i = 0; i < 5; i++){
+               let one_issue_link = allIssues[i].href;
+               let full_issue_link = "https://github.com" + one_issue_link;
+               //console.log(full_issue_link);
+               let data = JSON.stringify(full_issue_link);
+               let file_present = fs.existsSync("GithubScrap.json");
+
+               if(file_present){
+                   fs.appendFileSync("GithubScrap.json", data);
+                   fs.appendFileSync("GithubScrap.json", "\n");
+                }else{
+                    fs.writeFileSync("GithubScrap.json", data);
+                    fs.appendFileSync("GithubScrap.json", "\n");
+                }
+            }
+        }
     }
 }
